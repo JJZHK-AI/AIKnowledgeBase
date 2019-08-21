d4ada2555eadcc6b137a2012cb114daae686643c
JJZHK <jeffcobile@gmail.com>
2019-08-21 14:13:03 +0800

    Initial commit

---
+++ ELib/pyn/cnn/functions.py
@@ -0,0 +1,30 @@
+'''
+@author: zhangkai
+@license: (C) Copyright 2017-2023
+@contact: jeffcobile@gmail.com
+@Software : PyCharm
+@file: functions.py
+@time: 2019-07-11 14:08:29
+@desc: 
+'''
+import cv2
+import numpy as np
+def preprocess_image(cv2im, resize_im=True):
+    # mean and std list for channels (Imagenet)
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
+    # Resize image
+    if resize_im:
+        cv2im = cv2.resize(cv2im, (224, 224))
+    im_as_arr = np.float32(cv2im)
+    im_as_arr = np.ascontiguousarray(im_as_arr[..., ::-1])
+    im_as_arr = im_as_arr.transpose(2, 0, 1)  # Convert array to D,W,H
+    # Normalize the channels
+    for channel, _ in enumerate(im_as_arr):
+        im_as_arr[channel] /= 255
+        im_as_arr[channel] -= mean[channel]
+        im_as_arr[channel] /= std[channel]
+    # Convert to float tensor
+    im_as_ten = im_as_arr.astype(dtype=float)
+    im_as_var = np.expand_dims(im_as_ten, axis=0)
+    return im_as_var
