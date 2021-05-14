ac536ebed303d3525cc810e364726f7fa9123f3a
JJZHK <jeffcobile@gmail.com>
2021-05-14 15:49:25 +0800

    ✨ Transformer实现

---
+++ 05.自然语言处理/lib/Attention.py
@@ -0,0 +1,29 @@
+import math
+import torch
+
+def attention(query, key, value, mask=None, dropout=None):
+    """注意力机制的实现, 输入分别是query, key, value, mask: 掩码张量, 
+       dropout是nn.Dropout层的实例化对象, 默认为None"""
+    # 在函数中, 首先取query的最后一维的大小, 一般情况下就等同于我们的词嵌入维度, 命名为d_k
+    d_k = query.size(-1)
+    # 按照注意力公式, 将query与key的转置相乘, 这里面key是将最后两个维度进行转置, 再除以缩放系数根号下d_k, 这种计算方法也称为缩放点积注意力计算.
+    # 得到注意力得分张量scores
+    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+
+    # 接着判断是否使用掩码张量
+    if mask is not None:
+        # 使用tensor的masked_fill方法, 将掩码张量和scores张量每个位置一一比较, 如果掩码张量处为0
+        # 则对应的scores张量用-1e9这个值来替换, 如下演示
+        scores = scores.masked_fill(mask == 0, -1e9)
+
+    # 对scores的最后一维进行softmax操作, 使用F.softmax方法, 第一个参数是softmax对象, 第二个是目标维度.
+    # 这样获得最终的注意力张量
+    p_attn = torch.nn.functional.softmax(scores, dim = -1)
+
+    # 之后判断是否使用dropout进行随机置0
+    if dropout is not None:
+        # 将p_attn传入dropout对象中进行'丢弃'处理
+        p_attn = dropout(p_attn)
+
+    # 最后, 根据公式将p_attn与value张量相乘获得最终的query注意力表示, 同时返回注意力张量
+    return torch.matmul(p_attn, value), p_attn
---
+++ 05.自然语言处理/lib/PositionalEncoding.py
@@ -0,0 +1,50 @@
+import torch
+import math
+
+# 定义位置编码器类, 我们同样把它看做一个层, 因此会继承nn.Module    
+class PositionalEncoding(torch.nn.Module):
+    def __init__(self, d_model, dropout, max_len=5000):
+        """位置编码器类的初始化函数, 共有三个参数, 分别是d_model: 词嵌入维度, 
+           dropout: 置0比率, max_len: 每个句子的最大长度"""
+        super(PositionalEncoding, self).__init__()
+
+        # 实例化nn中预定义的Dropout层, 并将dropout传入其中, 获得对象self.dropout
+        self.dropout = torch.nn.Dropout(p=dropout)
+
+        # 初始化一个位置编码矩阵, 它是一个0阵，矩阵的大小是max_len x d_model.
+        pe = torch.zeros(max_len, d_model)
+
+        # 初始化一个绝对位置矩阵, 在我们这里，词汇的绝对位置就是用它的索引去表示. 
+        # 所以我们首先使用arange方法获得一个连续自然数向量，然后再使用unsqueeze方法拓展向量维度使其成为矩阵， 
+        # 又因为参数传的是1，代表矩阵拓展的位置，会使向量变成一个max_len x 1 的矩阵， 
+        position = torch.arange(0, max_len).unsqueeze(1)
+
+        # 绝对位置矩阵初始化之后，接下来就是考虑如何将这些位置信息加入到位置编码矩阵中，
+        # 最简单思路就是先将max_len x 1的绝对位置矩阵， 变换成max_len x d_model形状，然后覆盖原来的初始位置编码矩阵即可， 
+        # 要做这种矩阵变换，就需要一个1xd_model形状的变换矩阵div_term，我们对这个变换矩阵的要求除了形状外，
+        # 还希望它能够将自然数的绝对位置编码缩放成足够小的数字，有助于在之后的梯度下降过程中更快的收敛.  这样我们就可以开始初始化这个变换矩阵了.
+        # 首先使用arange获得一个自然数矩阵， 但是细心的同学们会发现， 我们这里并没有按照预计的一样初始化一个1xd_model的矩阵， 
+        # 而是有了一个跳跃，只初始化了一半即1xd_model/2 的矩阵。 为什么是一半呢，其实这里并不是真正意义上的初始化了一半的矩阵，
+        # 我们可以把它看作是初始化了两次，而每次初始化的变换矩阵会做不同的处理，第一次初始化的变换矩阵分布在正弦波上， 第二次初始化的变换矩阵分布在余弦波上， 
+        # 并把这两个矩阵分别填充在位置编码矩阵的偶数和奇数位置上，组成最终的位置编码矩阵.
+        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        # 这样我们就得到了位置编码矩阵pe, pe现在还只是一个二维矩阵，要想和embedding的输出（一个三维张量）相加，
+        # 就必须拓展一个维度，所以这里使用unsqueeze拓展维度.
+        pe = pe.unsqueeze(0)
+
+        # 最后把pe位置编码矩阵注册成模型的buffer，什么是buffer呢，
+        # 我们把它认为是对模型效果有帮助的，但是却不是模型结构中超参数或者参数，不需要随着优化步骤进行更新的增益对象. 
+        # 注册之后我们就可以在模型保存后重加载时和模型结构与参数一同被加载.
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        """forward函数的参数是x, 表示文本序列的词嵌入表示"""
+        # 在相加之前我们对pe做一些适配工作， 将这个三维张量的第二维也就是句子最大长度的那一维将切片到与输入的x的第二维相同即x.size(1)，
+        # 因为我们默认max_len为5000一般来讲实在太大了，很难有一条句子包含5000个词汇，所以要进行与输入张量的适配. 
+        # 最后使用Variable进行封装，使其与x的样式相同，但是它是不需要进行梯度求解的，因此把requires_grad设置成false.
+        x = x + torch.autograd.Variable(self.pe[:, :x.size(1)], requires_grad=False)
+        # 最后使用self.dropout对象进行'丢弃'操作, 并返回结果.
+        return self.dropout(x)
